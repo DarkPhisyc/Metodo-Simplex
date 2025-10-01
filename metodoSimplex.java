2383e284823fda521255270667e92e7d961d004e
DarkPhisyc <darkphisyc50@gmail.com>
2025-10-01 10:56:15 -0600

    puse un system

--- metodoSimplex.java
+++ metodoSimplex.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class metodoSimplex {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println();
     }
 }
